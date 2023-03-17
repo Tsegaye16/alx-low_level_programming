c5e1a1df27f49a9bc4375f2a10a2d8dd4353b977
Addisu Abewa <tsegayeabewa@gmail.com>
2023-03-17 10:38:56 -0700

    3-print alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
