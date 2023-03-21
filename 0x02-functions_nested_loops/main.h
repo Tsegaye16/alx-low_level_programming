acc9639c7f2bec80b400c657cd923c414d3bebfd
Addisu Abewa <tsegayeabewa@gmail.com>
2023-03-21 13:30:27 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#ifndef main_h
+#define main_h
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -11,3 +13,5 @@ void times_table(void);
 int add(int,int);
 void print_to_98(int n);
 void print_times_table(int n);
+
+#endif
